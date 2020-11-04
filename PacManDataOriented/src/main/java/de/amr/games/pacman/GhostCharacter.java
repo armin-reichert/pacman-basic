4b07c9a082b17cd3b024ca58cf7d31418d9cd40e
Armin Reichert <armin.reichert@web.de>
2020-11-04 16:04:13 +0100

    moved

---
+++ PacManDataOriented/src/main/java/de/amr/games/pacman/GhostCharacter.java
@@ -0,0 +1,12 @@
+package de.amr.games.pacman;
+
+public enum GhostCharacter {
+	/** Blinky, the red ghost */
+	OIKAKE,
+	/** Pinky, the pinky gost */
+	MACHIBUSE,
+	/** Inky, the blue ghost */
+	KIMAGURE,
+	/** Clyde, the orange ghost */
+	OTOBOKE;
+}
