04e135240239272c134c1eb8058593a688a3ee13
Armin Reichert <armin.reichert@web.de>
2020-12-07 13:37:08 +0100

    refactoring

--- PacManDataOriented/src/main/java/de/amr/games/pacman/GameClock.java
+++ PacManDataOriented/src/main/java/de/amr/games/pacman/GameClock.java
@@ -7,9 +7,6 @@ public class GameClock {
 	public long frames;
 	public long framesTotal;
 	public long fpsCountStartTime;
-	public long frameStartTime;
-	public long frameEndTime;
-	public long frameDuration;
 
 	public int sec(float seconds) {
 		return (int) (seconds * targetFPS);
@@ -26,10 +23,10 @@ public class GameClock {
 	}
 
 	public void tick(Runnable frame) {
-		frameStartTime = System.nanoTime();
+		long frameStartTime = System.nanoTime();
 		frame.run();
-		frameEndTime = System.nanoTime();
-		frameDuration = frameEndTime - frameStartTime;
+		long frameEndTime = System.nanoTime();
+		long frameDuration = frameEndTime - frameStartTime;
 		++frames;
 		++framesTotal;
 		if (frameEndTime - fpsCountStartTime >= 1_000_000_000) {
