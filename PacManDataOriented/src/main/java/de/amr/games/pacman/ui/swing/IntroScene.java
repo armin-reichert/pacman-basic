ca05b5afb8311ed82357aab3d8c9fded74f1378d
Armin Reichert <armin.reichert@web.de>
2020-12-07 12:33:45 +0100

    refactoring

--- PacManDataOriented/src/main/java/de/amr/games/pacman/ui/swing/IntroScene.java
+++ PacManDataOriented/src/main/java/de/amr/games/pacman/ui/swing/IntroScene.java
@@ -32,8 +32,10 @@ class IntroScene {
 	public PacManGame game;
 	public Assets assets;
 	public Dimension size;
-	public long timer;
+	public long passed;
+	public long mark;
 	public int pacManX;
+	public int leadingGhostX;
 	public boolean chasingPacMan;
 
 	public IntroScene(PacManGame game, Assets assets, Dimension size) {
@@ -44,46 +46,48 @@ class IntroScene {
 	}
 
 	public void reset() {
-		timer = 0;
+		passed = 0;
+		mark = 0;
 		pacManX = size.width;
+		leadingGhostX = pacManX + 24;
 		chasingPacMan = true;
 	}
 
 	public void draw(Graphics2D g) {
 		g.setFont(assets.scoreFont);
 
-		int time = 1;
-		if (timer >= game.clock.sec(time)) {
+		mark = 1;
+		if (passed >= game.clock.sec(mark)) {
 			int w = assets.imageLogo.getWidth();
 			g.drawImage(assets.imageLogo, (size.width - w) / 2, 3, null);
 		}
 
-		time += 1;
-		if (timer >= game.clock.sec(time)) {
+		mark += 1;
+		if (passed >= game.clock.sec(mark)) {
 			g.setColor(Color.WHITE);
 			drawCenteredText(g, "CHARACTER / NICKNAME", 8 * TS, size.width);
 		}
 
-		time += 1;
+		mark += 1;
 		for (int ghost = 0; ghost <= 3; ++ghost) {
 			int y = (10 + 3 * ghost) * TS;
-			if (timer >= game.clock.sec(time)) {
+			if (passed >= game.clock.sec(mark)) {
 				BufferedImage ghostLookingRight = assets.sheet(0, 4 + ghost);
 				g.drawImage(ghostLookingRight, 2 * TS - 3, y - 2, null);
 			}
-			if (timer >= game.clock.sec(time + 0.5f)) {
+			if (passed >= game.clock.sec(mark + 0.5f)) {
 				String character = (String) GHOST_INTRO_TEXTS[ghost][0];
 				String nickname = (String) GHOST_INTRO_TEXTS[ghost][1];
 				Color color = (Color) GHOST_INTRO_TEXTS[ghost][2];
 				String text = "-";
-				text += timer > game.clock.sec(time + 1) ? character + "    " + nickname : character;
+				text += passed > game.clock.sec(mark + 1) ? character + "    " + nickname : character;
 				g.setColor(color);
 				g.drawString(text, 4 * TS, y + 11);
 			}
-			time += 2;
+			mark += 2;
 		}
 
-		if (timer >= game.clock.sec(time)) {
+		if (passed >= game.clock.sec(mark)) {
 			g.setColor(Color.PINK);
 			g.fillRect(9 * TS + 6, 27 * TS + 2, 2, 2);
 			drawCenteredText(g, "10 PTS", 28 * TS, size.width);
@@ -93,53 +97,49 @@ class IntroScene {
 			drawCenteredText(g, "50 PTS", 30 * TS, size.width);
 		}
 
-		time += 1;
-		if (timer >= game.clock.sec(time)) {
+		mark += 1;
+		if (passed >= game.clock.sec(mark)) {
 			if (chasingPacMan) {
-				drawChasingPacMan(g, time);
+				drawChasingPacMan(g);
 			} else {
-				drawChasingGhosts(g, time);
+				drawChasingGhosts(g);
 			}
 		}
 
-		time += 1;
-		if (timer >= game.clock.sec(time)) {
+		mark += 1;
+		if (passed >= game.clock.sec(mark)) {
 			g.setColor(Color.WHITE);
 			game.clock.alternating(20, () -> {
 				drawCenteredText(g, "Press SPACE to play!", size.height - 20, size.width);
 			});
 		}
 
-		++timer;
-		if (timer == game.clock.sec(30)) {
+		++passed;
+		if (passed == game.clock.sec(30)) {
 			reset();
 		}
 	}
 
-	private void drawChasingPacMan(Graphics2D g, int time) {
-		int x = pacManX;
+	private void drawChasingPacMan(Graphics2D g) {
 		int y = 22 * TS;
 		game.clock.alternating(20, () -> {
 			g.setColor(Color.PINK);
 			g.fillOval(2 * TS, y + 2, 10, 10);
 		});
-		g.drawImage(pacManWalking(LEFT), x, y, null);
-		x += 24;
-		g.drawImage(ghostWalking(LEFT, BLINKY), x, y, null);
-		x += 16;
-		g.drawImage(ghostWalking(LEFT, PINKY), x, y, null);
-		x += 16;
-		g.drawImage(ghostWalking(LEFT, INKY), x, y, null);
-		x += 16;
-		g.drawImage(ghostWalking(LEFT, CLYDE), x, y, null);
+		g.drawImage(pacManWalking(LEFT), pacManX, y, null);
+		g.drawImage(ghostWalking(LEFT, BLINKY), leadingGhostX, y, null);
+		g.drawImage(ghostWalking(LEFT, PINKY), leadingGhostX + 16, y, null);
+		g.drawImage(ghostWalking(LEFT, INKY), leadingGhostX + 2 * 16, y, null);
+		g.drawImage(ghostWalking(LEFT, CLYDE), leadingGhostX + 3 * 16, y, null);
 		if (pacManX > 2 * TS) {
 			pacManX -= 1;
+			leadingGhostX -= 1;
 		} else {
 			chasingPacMan = false;
 		}
 	}
 
-	private void drawChasingGhosts(Graphics2D g, int time) {
+	private void drawChasingGhosts(Graphics2D g) {
 		int x = pacManX;
 		int y = 22 * TS;
 		g.drawImage(pacManWalking(RIGHT), x, y, null);
