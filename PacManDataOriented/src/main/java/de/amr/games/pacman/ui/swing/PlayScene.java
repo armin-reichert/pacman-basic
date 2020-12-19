68c2c783c81c9861179cec8923f6d7609febfb04
Armin Reichert <armin.reichert@web.de>
2020-12-19 13:03:18 +0100

    cleanup

--- PacManDataOriented/src/main/java/de/amr/games/pacman/ui/swing/PlayScene.java
+++ PacManDataOriented/src/main/java/de/amr/games/pacman/ui/swing/PlayScene.java
@@ -29,7 +29,11 @@ import de.amr.games.pacman.lib.V2i;
  */
 class PlayScene {
 
-	final Polygon TRIANGLE = new Polygon(new int[] { -4, 4, 0 }, new int[] { 0, 0, 4 }, 3);
+	static int t(int nTiles) {
+		return nTiles * TS;
+	}
+
+	static final Polygon TRIANGLE = new Polygon(new int[] { -4, 4, 0 }, new int[] { 0, 0, 4 }, 3);
 
 	public boolean debugMode;
 
@@ -52,8 +56,8 @@ class PlayScene {
 		drawLevelCounter(g);
 		drawMaze(g);
 		drawPacMan(g);
-		for (int ghost = 0; ghost < 4; ++ghost) {
-			drawGhost(g, ghost);
+		for (int ghostID = 0; ghostID < 4; ++ghostID) {
+			drawGhost(g, ghostID);
 		}
 		drawMessage(g);
 		drawDebugInfo(g);
@@ -72,7 +76,7 @@ class PlayScene {
 		if (messageText != null) {
 			g.setFont(assets.scoreFont);
 			g.setColor(messageColor);
-			drawCenteredText(g, messageText, 21 * TS, size.x);
+			drawCenteredText(g, messageText, t(21), size.x);
 		}
 	}
 
@@ -80,52 +84,52 @@ class PlayScene {
 		g.setFont(assets.scoreFont);
 		g.translate(0, 2);
 		g.setColor(Color.WHITE);
-		g.drawString("SCORE", 1 * TS, 1 * TS);
-		g.drawString("HIGH SCORE", 16 * TS, 1 * TS);
+		g.drawString("SCORE", t(1), t(1));
+		g.drawString("HIGH SCORE", t(16), t(1));
 		g.translate(0, 1);
 		g.setColor(Color.YELLOW);
-		g.drawString(String.format("%08d", game.score), 1 * TS, 2 * TS);
+		g.drawString(String.format("%08d", game.score), t(1), t(2));
 		g.setColor(Color.LIGHT_GRAY);
-		g.drawString(String.format("L%02d", game.level), 9 * TS, 2 * TS);
+		g.drawString(String.format("L%02d", game.level), t(9), t(2));
 		g.setColor(Color.YELLOW);
-		g.drawString(String.format("%08d", game.hiscore.points), 16 * TS, 2 * TS);
+		g.drawString(String.format("%08d", game.hiscore.points), t(16), t(2));
 		g.setColor(Color.LIGHT_GRAY);
-		g.drawString(String.format("L%02d", game.hiscore.level), 24 * TS, 2 * TS);
+		g.drawString(String.format("L%02d", game.hiscore.level), t(24), t(2));
 		g.translate(0, -3);
 	}
 
 	private void drawLivesCounter(Graphics2D g) {
-		int y = size.y - 2 * TS;
+		int y = size.y - t(2);
 		for (int i = 0; i < Math.min(game.lives, 4); ++i) {
-			g.drawImage(assets.imageLive, 2 * (i + 1) * TS, y, null);
+			g.drawImage(assets.imageLive, t(2 * (i + 1)), y, null);
 		}
 		if (game.lives > 4) {
 			g.setColor(Color.YELLOW);
 			g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 8));
-			g.drawString("+" + (game.lives - 4), 10 * TS, y + TS);
+			g.drawString("+" + (game.lives - 4), t(10), y + t(1));
 		}
 	}
 
 	private void drawLevelCounter(Graphics2D g) {
-		int x = (WORLD_TILES.x - 4) * TS;
+		int x = t(WORLD_TILES.x - 4);
 		int first = Math.max(1, game.level - 6);
 		for (int level = first; level <= game.level; ++level) {
 			BufferedImage symbol = assets.symbols[game.level(level).bonusSymbol];
-			g.drawImage(symbol, x, size.y - 2 * TS, null);
-			x -= 2 * TS;
+			g.drawImage(symbol, x, size.y - t(2), null);
+			x -= t(2);
 		}
 	}
 
 	private void hideTile(Graphics2D g, int x, int y) {
 		g.setColor(Color.BLACK);
-		g.fillRect(x * TS, y * TS, TS, TS);
+		g.fillRect(t(x), t(y), TS, TS);
 	}
 
 	private void drawMazeFlashing(Graphics2D g) {
 		game.clock.runAlternating(game.clock.sec(0.25f), () -> {
-			g.drawImage(assets.imageMazeEmpty, 0, 3 * TS, null);
+			g.drawImage(assets.imageMazeEmpty, 0, t(3), null);
 		}, () -> {
-			g.drawImage(assets.imageMazeEmptyWhite, 0, 3 * TS, null);
+			g.drawImage(assets.imageMazeEmptyWhite, 0, t(3), null);
 		}, () -> {
 			game.mazeFlashesRemaining--;
 		});
@@ -136,7 +140,7 @@ class PlayScene {
 			drawMazeFlashing(g);
 			return;
 		}
-		g.drawImage(assets.imageMazeFull, 0, 3 * TS, null);
+		g.drawImage(assets.imageMazeFull, 0, t(3), null);
 		for (int x = 0; x < WORLD_TILES.x; ++x) {
 			for (int y = 0; y < WORLD_TILES.y; ++y) {
 				if (game.world.foodEatenAt(x, y)) {
@@ -151,10 +155,10 @@ class PlayScene {
 			}
 		}
 		if (game.bonusAvailableTimer > 0) {
-			g.drawImage(assets.symbols[game.level().bonusSymbol], 13 * TS, 20 * TS - HTS, null);
+			g.drawImage(assets.symbols[game.level().bonusSymbol], t(13), t(20) - HTS, null);
 		} else if (game.bonusConsumedTimer > 0) {
 			BufferedImage image = assets.numbers.get(game.level().bonusPoints);
-			g.drawImage(image, (size.x - image.getWidth()) / 2, 20 * TS - HTS, null);
+			g.drawImage(image, (size.x - image.getWidth()) / 2, t(20) - HTS, null);
 		}
 		if (debugMode) {
 			drawMazeStructure(g);
@@ -219,7 +223,7 @@ class PlayScene {
 		} else {
 			sprite = assets.sheet(2 * dir + walking, 4 + ghostIndex);
 		}
-		g.drawImage(sprite, (int) ghost.position.x - HTS, (int) ghost.position.y - HTS, null);
+		g.drawImage(sprite, round(ghost.position.x - HTS), round(ghost.position.y - HTS), null);
 	}
 
 	private void drawDebugInfo(Graphics2D g) {
@@ -229,14 +233,14 @@ class PlayScene {
 			String stateText = String.format("%s (%s)", game.stateDescription(), ticksText);
 			g.setColor(Color.WHITE);
 			g.setFont(new Font("Arial", Font.PLAIN, 6));
-			g.drawString(stateText, 1 * TS, 3 * TS);
+			g.drawString(stateText, t(1), t(3));
 			for (Ghost ghost : game.ghosts) {
 				g.setColor(Color.WHITE);
-				g.drawRect((int) ghost.position.x, (int) ghost.position.y, TS, TS);
+				g.drawRect(round(ghost.position.x), round(ghost.position.y), TS, TS);
 				if (ghost.targetTile != null) {
 					Color c = Assets.GHOST_COLORS[ghost.id];
 					g.setColor(c);
-					g.fillRect(ghost.targetTile.x * TS + HTS / 2, ghost.targetTile.y * TS + HTS / 2, HTS, HTS);
+					g.fillRect(t(ghost.targetTile.x) + HTS / 2, t(ghost.targetTile.y) + HTS / 2, HTS, HTS);
 				}
 			}
 		}
@@ -255,12 +259,12 @@ class PlayScene {
 						if (game.world.isWall(nx, ny)) {
 							continue;
 						}
-						g.drawLine(x * TS + HTS, y * TS + HTS, nx * TS + HTS, ny * TS + HTS);
+						g.drawLine(t(x) + HTS, t(y) + HTS, t(nx) + HTS, t(ny) + HTS);
 					}
 				} else if (game.world.isUpwardsBlocked(x, y)) {
-					g.translate(x * TS + HTS, y * TS);
+					g.translate(t(x) + HTS, t(y));
 					g.fillPolygon(TRIANGLE);
-					g.translate(-(x * TS + HTS), -y * TS);
+					g.translate(-t(x) - HTS, -t(y));
 				}
 			}
 		}
