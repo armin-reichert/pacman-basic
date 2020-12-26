48ed12135d78aa001ad1734014180056618187a8
Armin Reichert <armin.reichert@web.de>
2020-12-26 09:34:18 +0100

    cleanup

--- pacman/src/main/java/de/amr/games/pacman/lib/Direction.java
+++ pacman/src/main/java/de/amr/games/pacman/lib/Direction.java
@@ -1,30 +1,20 @@
 package de.amr.games.pacman.lib;
 
 /**
- * The possible move directions inside the world.
+ * The move directions inside the world.
  * 
  * @author Armin Reichert
- *
  */
 public enum Direction {
 
 	LEFT(-1, 0), RIGHT(1, 0), UP(0, -1), DOWN(0, 1);
 
+	private static final Direction[] OPPOSITE = { RIGHT, LEFT, DOWN, UP };
+
 	public final V2i vec;
 
 	public Direction opposite() {
-		switch (this) {
-		case LEFT:
-			return RIGHT;
-		case RIGHT:
-			return LEFT;
-		case UP:
-			return DOWN;
-		case DOWN:
-			return UP;
-		default:
-			throw new IllegalStateException();
-		}
+		return OPPOSITE[ordinal()];
 	}
 
 	private Direction(int x, int y) {
