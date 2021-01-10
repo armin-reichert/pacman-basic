d8a074f644fc7ee07b298dfe9f33348be6aa7a4d
Armin Reichert <armin.reichert@web.de>
2021-01-10 17:11:40 +0100

    cleanup

--- pacman/src/main/java/de/amr/games/pacman/worlds/AbstractPacManGameWorld.java
+++ pacman/src/main/java/de/amr/games/pacman/worlds/AbstractPacManGameWorld.java
@@ -42,16 +42,15 @@ public abstract class AbstractPacManGameWorld implements PacManGameWorld {
 	}
 
 	protected byte[][] readMapFile(String path) {
-		int size_x = 28, size_y = 36;
-		byte[][] map = new byte[size_y][size_x];
+		byte[][] map = new byte[size.y][size.x];
 		try (InputStream is = getClass().getResourceAsStream(path)) {
 			if (is == null) {
 				throw new RuntimeException("Resource not found: " + path);
 			}
 			BufferedReader rdr = new BufferedReader(new InputStreamReader(is));
-			for (int y = 0; y < size_y; ++y) {
+			for (int y = 0; y < size.y; ++y) {
 				String line = rdr.readLine();
-				for (int x = 0; x < size_x; ++x) {
+				for (int x = 0; x < size.x; ++x) {
 					map[y][x] = decode(line.charAt(x));
 				}
 			}
