1a2f931975fe6779079369e784d587e321093186
Armin Reichert <armin.reichert@web.de>
2021-01-12 07:35:25 +0100

    cmd line switch for classic Pac-Man vs. Ms. Pac-Man

--- pacman/src/main/java/de/amr/games/pacman/PacManGameApp.java
+++ pacman/src/main/java/de/amr/games/pacman/PacManGameApp.java
@@ -5,6 +5,7 @@ import static java.awt.EventQueue.invokeLater;
 import de.amr.games.pacman.core.PacManGame;
 import de.amr.games.pacman.ui.PacManGameUI;
 import de.amr.games.pacman.ui.swing.PacManGameSwingUI;
+import de.amr.games.pacman.worlds.classic.PacManClassicWorld;
 import de.amr.games.pacman.worlds.mspacman.MsPacManWorld;
 
 /**
@@ -15,10 +16,16 @@ import de.amr.games.pacman.worlds.mspacman.MsPacManWorld;
 public class PacManGameApp {
 
 	public static void main(String[] args) {
-		float scaling = args.length > 0 ? Float.parseFloat(args[0]) : 2;
+		String variant = args.length > 0 ? args[0] : "classic";
+		float scaling = args.length > 1 ? Float.parseFloat(args[1]) : 2;
 		PacManGame game = new PacManGame();
-//		game.setWorld(new PacManClassicWorld());
-		game.setWorld(new MsPacManWorld(1));
+		if ("classic".equals(variant)) {
+			game.setWorld(new PacManClassicWorld());
+		} else if ("mspacman".equals(variant)) {
+			MsPacManWorld msPacmanWorld = new MsPacManWorld();
+			msPacmanWorld.setMapIndex(1);
+			game.setWorld(msPacmanWorld);
+		}
 		invokeLater(() -> {
 			PacManGameUI ui = new PacManGameSwingUI(game.world.sizeInTiles(), scaling);
 			ui.setGame(game);
