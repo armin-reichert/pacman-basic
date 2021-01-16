338179ce847600ecf776ed8490af489d66ffa272
Armin Reichert <armin.reichert@web.de>
2021-01-16 05:52:59 +0100

    doc

--- pacman/src/main/java/de/amr/games/pacman/creatures/Ghost.java
+++ pacman/src/main/java/de/amr/games/pacman/creatures/Ghost.java
@@ -22,16 +22,18 @@ public class Ghost extends Creature {
 	/** The current state of the ghost. */
 	public GhostState state;
 
-	/** The bounty for this killing ghost. */
+	/** The bounty earned for killing this ghost. */
 	public int bounty;
 
-	/** The individual counter of the ghost, used by the logic when ghosts can leave the house. */
+	/** The individual food counter, used to compute when the ghost can leave the house. */
 	public int dotCounter;
 
-	/** The "Cruise Elroy" mode of Blinky, the red ghost. */
+	/**
+	 * The "Cruise Elroy" mode of Blinky, the red ghost. Value is 1, 2 or -1, -2 (disabled Elroy mode).
+	 */
 	public byte elroyMode;
 
-	/** The tile that the ghost tries to move to. Can be inaccessible and outside of the maze. */
+	/** The tile that the ghost tries to reach. Can be inaccessible or outside of the maze. */
 	public V2i targetTile = V2i.NULL;
 
 	public Ghost(int id) {
