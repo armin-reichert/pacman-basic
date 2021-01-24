27b3c3b9763b23d242381d6d0aa4aac7e1b954b4
Armin Reichert <armin.reichert@web.de>
2021-01-24 12:51:46 +0100

    refac

--- pacman/src/main/java/de/amr/games/pacman/game/creatures/MsPacManBonus.java
+++ pacman/src/main/java/de/amr/games/pacman/game/creatures/MsPacManBonus.java
@@ -4,16 +4,9 @@ import static de.amr.games.pacman.game.worlds.MsPacManWorld.BONUS_POINTS;
 import static de.amr.games.pacman.lib.Direction.LEFT;
 import static de.amr.games.pacman.lib.Direction.RIGHT;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import de.amr.games.pacman.game.worlds.PacManGameWorld;
-import de.amr.games.pacman.lib.Direction;
-import de.amr.games.pacman.lib.V2i;
 
 public class MsPacManBonus extends ClassicBonus {
-
-	public Direction wanderingDirection;
 
 	public MsPacManBonus(PacManGameWorld world) {
 		super(world);
@@ -21,18 +14,21 @@ public class MsPacManBonus extends ClassicBonus {
 
 	@Override
 	public void activate(byte bonusSymbol, long ticks) {
-		visible = true;
 		symbol = bonusSymbol;
 		points = BONUS_POINTS[symbol];
 		edibleTicksLeft = ticks;
-		boolean entersMazeFromLeft = rnd.nextBoolean();
-		int portal = rnd.nextInt(world.numPortals());
-		V2i startTile = entersMazeFromLeft ? world.portalLeft(portal) : world.portalRight(portal);
-		placeAt(startTile, 0, 0);
-		wanderingDirection = entersMazeFromLeft ? RIGHT : LEFT;
-		dir = wishDir = wanderingDirection;
+		visible = true;
 		couldMove = true;
 		speed = 0.25f; // TODO what is the correct speed?
+		if (rnd.nextBoolean()) {
+			placeAt(world.portalLeft(rnd.nextInt(world.numPortals())), 0, 0);
+			targetTile = world.portalRight(rnd.nextInt(world.numPortals()));
+			dir = wishDir = RIGHT;
+		} else {
+			placeAt(world.portalRight(rnd.nextInt(world.numPortals())), 0, 0);
+			targetTile = world.portalLeft(rnd.nextInt(world.numPortals()));
+			dir = wishDir = LEFT;
+		}
 	}
 
 	@Override
@@ -45,7 +41,7 @@ public class MsPacManBonus extends ClassicBonus {
 	@Override
 	public void update() {
 		if (edibleTicksLeft > 0) {
-			wander();
+			headForTargetTile();
 			if (world.isPortal(tile())) {
 				edibleTicksLeft = 0;
 				visible = false;
@@ -58,18 +54,5 @@ public class MsPacManBonus extends ClassicBonus {
 				visible = false;
 			}
 		}
-	}
-
-	private void wander() {
-		V2i location = tile();
-		if (!couldMove || world.isIntersection(location)) {
-			List<Direction> dirs = accessibleDirections(location, dir.opposite()).collect(Collectors.toList());
-			if (dirs.size() > 1) {
-				// give random movement a bias towards the wandering direction
-				dirs.remove(wanderingDirection.opposite());
-			}
-			wishDir = dirs.get(rnd.nextInt(dirs.size()));
-		}
-		tryMoving();
 	}
 }
