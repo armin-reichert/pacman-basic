0f7cd8b8aee2241a0a403bc2c793919fd8a649c5
Armin Reichert <armin.reichert@web.de>
2021-01-27 09:20:57 +0100

    command-line

--- pacman/src/main/java/de/amr/games/pacman/PacManGameApp.java
+++ pacman/src/main/java/de/amr/games/pacman/PacManGameApp.java
@@ -1,9 +1,10 @@
 package de.amr.games.pacman;
 
+import static de.amr.games.pacman.lib.Logging.log;
 import static java.awt.EventQueue.invokeLater;
-import static java.lang.Float.parseFloat;
 
 import de.amr.games.pacman.game.core.PacManGameController;
+import de.amr.games.pacman.game.core.PacManGameModel;
 import de.amr.games.pacman.game.worlds.PacManGameWorld;
 import de.amr.games.pacman.ui.api.PacManGameUI;
 import de.amr.games.pacman.ui.swing.PacManGameSwingUI;
@@ -15,13 +16,70 @@ import de.amr.games.pacman.ui.swing.PacManGameSwingUI;
  */
 public class PacManGameApp {
 
+	static class Options {
+
+		float scaling = 2;
+		byte variant = PacManGameModel.MS_PACMAN;
+
+		static Options parse(String[] args) {
+			Options options = new Options();
+			int i = 0;
+			while (i < args.length) {
+				String arg = args[i];
+				if ("-pacman".equals(arg)) {
+					options.variant = PacManGameModel.CLASSIC;
+					++i;
+					continue;
+				}
+				if ("-mspacman".equals(arg)) {
+					options.variant = PacManGameModel.MS_PACMAN;
+					++i;
+					continue;
+				}
+				if ("-scaling".equals(arg)) {
+					if (i == args.length - 1) {
+						log("Error parsing command-line: missing scaling value.");
+						++i;
+						continue;
+					}
+					++i;
+					arg = args[i];
+					try {
+						options.scaling = Float.parseFloat(arg);
+					} catch (NumberFormatException x) {
+						log("Error parsing command-line: '%s' is no legal scaling value.", arg);
+					}
+					++i;
+					continue;
+				}
+				log("Error parsing command-line: Unknown argument '%s'", arg);
+				++i;
+			}
+			return options;
+		}
+	}
+
+	/**
+	 * Starter class.
+	 * 
+	 * @param args command-line arguments
+	 *             <ul>
+	 *             <li><code>-scaling</code> <em>value</em>: scaling of UI, default is 2</li>
+	 *             <li><code>-pacman</code>: start in Pac-Man mode</li>
+	 *             <li><code>-mspacman</code>: start in Ms. Pac-Man mode</li>
+	 *             </ul>
+	 */
 	public static void main(String[] args) {
-		float scaling = args.length > 1 ? parseFloat(args[1]) : 2;
+		Options options = Options.parse(args);
 		invokeLater(() -> {
 			PacManGameController gameController = new PacManGameController();
-			gameController.initMsPacManGame();
+			if (options.variant == PacManGameModel.CLASSIC) {
+				gameController.initPacManClassicGame();
+			} else {
+				gameController.initMsPacManGame();
+			}
 			PacManGameWorld world = gameController.game.world;
-			PacManGameUI ui = new PacManGameSwingUI(world.xTiles(), world.yTiles(), scaling);
+			PacManGameUI ui = new PacManGameSwingUI(world.xTiles(), world.yTiles(), options.scaling);
 			ui.setController(gameController);
 			ui.openWindow();
 			new Thread(gameController, "PacManGame").start();
