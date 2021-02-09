542cc9e0b027708313b93ab5db30b245ec1e527c
Armin Reichert <armin.reichert@web.de>
2021-02-09 09:37:49 +0100

    cleanup

--- pacman-ui-fx/pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/app/App.java
+++ pacman-ui-fx/pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/app/App.java
@@ -1,38 +1,31 @@
 package de.amr.games.pacman.ui.fx.app;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    private static Scene scene;
+	private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+	@Override
+	public void start(Stage stage) throws IOException {
+		Group group = new Group();
+		group.getChildren().add(new Text("Hello"));
+		scene = new Scene(group, 640, 480);
+		stage.setScene(scene);
+		stage.show();
+	}
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
+	public static void main(String[] args) {
+		launch();
+	}
 
 }
