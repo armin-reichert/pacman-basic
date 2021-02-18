154a46e98eedbc8dc8670d0c0a46ccec627934e1
Armin Reichert <armin.reichert@web.de>
2021-02-18 06:59:34 +0100

    getter

--- pacman/src/main/java/de/amr/games/pacman/lib/CountdownTimer.java
+++ pacman/src/main/java/de/amr/games/pacman/lib/CountdownTimer.java
@@ -5,6 +5,16 @@ public class CountdownTimer {
 	private long duration;
 	private long running;
 
+	public void tick() {
+		if (running < duration) {
+			++running;
+		}
+	}
+
+	public long getDuration() {
+		return duration;
+	}
+
 	public void setDuration(long ticks) {
 		duration = ticks;
 		running = 0;
@@ -17,12 +27,6 @@ public class CountdownTimer {
 
 	public long remaining() {
 		return duration == Long.MAX_VALUE ? Long.MAX_VALUE : duration - running;
-	}
-
-	public void tick() {
-		if (running < duration) {
-			++running;
-		}
 	}
 
 	public boolean expired() {
