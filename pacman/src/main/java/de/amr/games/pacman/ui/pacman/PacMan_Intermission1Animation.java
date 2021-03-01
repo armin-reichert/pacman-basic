46b46b8ff84daab9669324e9586b826095dcac3c
Armin Reichert <armin.reichert@web.de>
2021-03-01 10:32:57 +0100

    reuse intermission #1 animation

---
+++ pacman/src/main/java/de/amr/games/pacman/ui/pacman/PacMan_Intermission1Animation.java
@@ -0,0 +1,94 @@
+package de.amr.games.pacman.ui.pacman;
+
+import static de.amr.games.pacman.heaven.God.clock;
+import static de.amr.games.pacman.lib.Direction.RIGHT;
+import static de.amr.games.pacman.model.common.GhostState.FRIGHTENED;
+import static de.amr.games.pacman.model.common.GhostState.HUNTING_PAC;
+import static de.amr.games.pacman.world.PacManGameWorld.t;
+
+import de.amr.games.pacman.controller.PacManGameController;
+import de.amr.games.pacman.lib.Animation;
+import de.amr.games.pacman.lib.CountdownTimer;
+import de.amr.games.pacman.lib.Direction;
+import de.amr.games.pacman.model.common.Ghost;
+import de.amr.games.pacman.model.common.Pac;
+import de.amr.games.pacman.sound.PacManGameSound;
+import de.amr.games.pacman.sound.SoundManager;
+import de.amr.games.pacman.ui.animation.PacManGameAnimations;
+
+public class PacMan_Intermission1Animation {
+
+	public enum Phase {
+
+		BLINKY_CHASING_PACMAN, BIGPACMAN_CHASING_BLINKY;
+
+	}
+
+	public static final int groundY = t(20);
+
+	public final CountdownTimer timer = new CountdownTimer();
+	public final PacManGameController controller;
+	public final PacManGameAnimations animations;
+	public final SoundManager sounds;
+	public Ghost blinky;
+	public Pac pac;
+	public Phase phase;
+
+	public PacMan_Intermission1Animation(PacManGameController controller, PacManGameAnimations animations,
+			SoundManager sounds) {
+		this.controller = controller;
+		this.animations = animations;
+		this.sounds = sounds;
+	}
+
+	public void start() {
+		pac = new Pac("Pac-Man", Direction.LEFT);
+		pac.visible = true;
+		pac.setPosition(t(30), groundY);
+		pac.speed = 1.0f;
+		animations.playerAnimations().playerMunching(pac).forEach(Animation::restart);
+
+		blinky = new Ghost(0, "Blinky", Direction.LEFT);
+		blinky.visible = true;
+		blinky.state = HUNTING_PAC;
+		blinky.setPositionRelativeTo(pac, t(3), 0);
+		blinky.speed = pac.speed * 1.04f;
+		animations.ghostAnimations().ghostKicking(blinky, blinky.dir).restart();
+		animations.ghostAnimations().ghostFrightened(blinky, blinky.dir).restart();
+
+		sounds.loop(PacManGameSound.INTERMISSION_1, 2);
+
+		phase = Phase.BLINKY_CHASING_PACMAN;
+		timer.setDuration(clock.sec(5));
+	}
+
+	public void update() {
+		switch (phase) {
+		case BLINKY_CHASING_PACMAN:
+			if (timer.expired()) {
+				phase = Phase.BIGPACMAN_CHASING_BLINKY;
+				timer.setDuration(clock.sec(7));
+			}
+			break;
+		case BIGPACMAN_CHASING_BLINKY:
+			if (timer.running() == 0) {
+				blinky.setPosition(-t(2), groundY);
+				blinky.dir = blinky.wishDir = RIGHT;
+				blinky.speed = 1f;
+				blinky.state = FRIGHTENED;
+				pac.dir = RIGHT;
+				pac.speed = 1.3f;
+				pac.setPositionRelativeTo(blinky, -t(13), 0);
+			}
+			if (timer.expired()) {
+				controller.finishCurrentState();
+			}
+			break;
+		default:
+			break;
+		}
+		pac.move();
+		blinky.move();
+		timer.run();
+	}
+}
