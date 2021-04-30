17d985f50dd67e27d3ce93123d1ebcb3c18dea39
Armin Reichert <armin.reichert@web.de>
2021-04-30 08:47:51 +0200

    moved

--- pacman-ui-swing/src/main/java/de/amr/games/pacman/ui/swing/assets/AssetException.java
+++
@@ -1,8 +0,0 @@
-package de.amr.games.pacman.ui.swing.assets;
-
-public class AssetException extends RuntimeException {
-
-	public AssetException(String message, Object... args) {
-		super("Asset exception: " + String.format(message, args));
-	}
-}
