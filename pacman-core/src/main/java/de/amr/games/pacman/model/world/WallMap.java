30775e26cae7cee7079ffad669b86e181e24a753
Armin Reichert <armin.reichert@web.de>
2021-05-19 15:17:00 +0200

    wall map generator

--- pacman-core/src/main/java/de/amr/games/pacman/model/world/WallMap.java
+++ pacman-core/src/main/java/de/amr/games/pacman/model/world/WallMap.java
@@ -15,12 +15,12 @@ public interface WallMap {
 	public static final byte HORIZONTAL = 2;
 	public static final byte VERTICAL = 3;
 
-	public static void printWallMap(Writer w, WallMap wallMap, boolean symbols) {
-		byte[][] info = wallMap.info();
+	default void print(Writer w, boolean symbols) {
+		byte[][] info = info();
 		try (PrintWriter p = new PrintWriter(w)) {
 			for (int y = 0; y < info.length; ++y) {
 				for (int x = 0; x < info[0].length; ++x) {
-					p.print(symbols ? symbol(info[y][x]) : info[y][x]);
+					p.print(symbols ? String.valueOf(symbol(get(x, y))) : get(x, y));
 				}
 				p.println();
 			}
--- pacman-core/src/main/java/de/amr/games/pacman/model/world/WallMapGenerator.java
+++ pacman-core/src/main/java/de/amr/games/pacman/model/world/WallMapGenerator.java
@@ -1,20 +1,39 @@
 package de.amr.games.pacman.model.world;
 
-import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class WallMapGenerator {
 
 	public static void main(String[] args) {
-		new WallMapGenerator().run();
+		WallMapGenerator gen = new WallMapGenerator();
+		int resolution = 8;
+		gen.run("/pacman/maps/map1.txt", "pacman-map1-%d.txt", resolution);
+		gen.run("/mspacman/maps/map1.txt", "mspacman-map1-%d.txt", resolution);
+		gen.run("/mspacman/maps/map2.txt", "mspacman-map2-%d.txt", resolution);
+		gen.run("/mspacman/maps/map3.txt", "mspacman-map3-%d.txt", resolution);
+		gen.run("/mspacman/maps/map4.txt", "mspacman-map4-%d.txt", resolution);
 	}
 
-	private void run() {
-		WorldMap map = WorldMap.load("/pacman/maps/map1.txt");
+	private void run(String mapPath, String outputFileName, int resolution) {
+		File dir = new File(System.getProperty("user.dir"));
+		File out = new File(dir, String.format(outputFileName, resolution));
+		try {
+			out.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		WorldMap map = WorldMap.load(mapPath);
 		MapBasedPacManGameWorld world = new MapBasedPacManGameWorld();
 		world.setMap(map);
-		WallScanner scanner = new WallScanner(8);
-		WallMap wallMap = scanner.scan(world);
-		WallMap.printWallMap(new PrintWriter(System.out), wallMap, true);
+		WallMap wallMap = new WallScanner(resolution).scan(world);
+		try (FileWriter w = new FileWriter(out, StandardCharsets.UTF_8)) {
+			wallMap.print(w, true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
