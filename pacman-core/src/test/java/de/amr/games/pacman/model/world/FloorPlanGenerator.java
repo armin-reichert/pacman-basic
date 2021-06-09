233f02d09921643419affa4b9f5581a4850a9f33
Armin Reichert <armin.reichert@web.de>
2021-06-09 08:03:18 +0200

    improved

--- pacman-core/src/test/java/de/amr/games/pacman/model/world/FloorPlanGenerator.java
+++ pacman-core/src/test/java/de/amr/games/pacman/model/world/FloorPlanGenerator.java
@@ -5,36 +5,33 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * Test for floor plan generation.
+ * 
+ * @author Armin Reichert
+ */
 public class FloorPlanGenerator {
 
 	public static void main(String[] args) {
-		FloorPlanGenerator gen = new FloorPlanGenerator();
 		int resolution = 8;
-		gen.run("/pacman/maps/map1.txt", "pacman-map1-res-%d.txt", resolution);
-		gen.run("/mspacman/maps/map1.txt", "mspacman-map1-res-%d.txt", resolution);
-		gen.run("/mspacman/maps/map2.txt", "mspacman-map2-res-%d.txt", resolution);
-		gen.run("/mspacman/maps/map3.txt", "mspacman-map3-res-%d.txt", resolution);
-		gen.run("/mspacman/maps/map4.txt", "mspacman-map4-res-%d.txt", resolution);
+		File dir = new File(System.getProperty("user.dir"));
+		createFloorPlan("/pacman/maps/map1.txt", dir, "floorplan-pacman-map1-res-%d.txt", resolution);
+		createFloorPlan("/mspacman/maps/map1.txt", dir, "floorplan-mspacman-map1-res-%d.txt", resolution);
+		createFloorPlan("/mspacman/maps/map2.txt", dir, "floorplan-mspacman-map2-res-%d.txt", resolution);
+		createFloorPlan("/mspacman/maps/map3.txt", dir, "floorplan-mspacman-map3-res-%d.txt", resolution);
 	}
 
-	private void run(String mapPath, String outputFileName, int resolution) {
-		File dir = new File(System.getProperty("user.dir"));
-		File out = new File(dir, String.format(outputFileName, resolution));
-		try {
-			out.createNewFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
-		WorldMap map = WorldMap.load(mapPath);
+	private static void createFloorPlan(String mapPath, File dir, String outputFileNamePattern, int resolution) {
 		MapBasedPacManGameWorld world = new MapBasedPacManGameWorld();
+		WorldMap map = WorldMap.load(mapPath);
 		world.setMap(map);
 		FloorPlan wallMap = new FloorPlanBuilder(resolution).build(world);
+		File out = new File(dir, String.format(outputFileNamePattern, resolution));
 		try (FileWriter w = new FileWriter(out, StandardCharsets.UTF_8)) {
 			wallMap.print(w, true);
+			System.out.println("Floor plan " + out.getAbsolutePath() + " created");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println("Wall map file " + out.getAbsolutePath() + " created");
 	}
 }
