270e98b011ed0516afea15b9f64ec96658f79143
Armin Reichert <armin.reichert@web.de>
2021-12-23 12:41:11 +0100

    use FSM class

--- pacman-core/src/main/java/de/amr/games/pacman/controller/mspacman/Intermission2Controller.java
+++ pacman-core/src/main/java/de/amr/games/pacman/controller/mspacman/Intermission2Controller.java
@@ -26,8 +26,9 @@ package de.amr.games.pacman.controller.mspacman;
 import static de.amr.games.pacman.model.world.PacManGameWorld.t;
 
 import de.amr.games.pacman.controller.PacManGameController;
+import de.amr.games.pacman.controller.mspacman.Intermission2Controller.IntermissionState;
 import de.amr.games.pacman.lib.Direction;
-import de.amr.games.pacman.lib.TickTimer;
+import de.amr.games.pacman.lib.FiniteStateMachine;
 import de.amr.games.pacman.model.common.Pac;
 import de.amr.games.pacman.model.mspacman.entities.Flap;
 
@@ -39,121 +40,108 @@ import de.amr.games.pacman.model.mspacman.entities.Flap;
  * 
  * @author Armin Reichert
  */
-public abstract class Intermission2Controller {
+public abstract class Intermission2Controller extends FiniteStateMachine<IntermissionState> {
 
-	public enum Phase {
-
+	public enum IntermissionState {
 		FLAP, ACTION;
 	}
 
 	public static final int UPPER_Y = t(12), LOWER_Y = t(24), MIDDLE_Y = t(18);
 
 	public final PacManGameController gameController;
-	public final TickTimer timer = new TickTimer(getClass().getSimpleName() + "-timer");
-	public Phase phase;
 	public Flap flap;
 	public Pac pacMan, msPacMan;
 
-	public void enter(Phase newPhase) {
-		phase = newPhase;
-		timer.reset();
-		timer.start();
-	}
-
 	public Intermission2Controller(PacManGameController gameController) {
+		super(IntermissionState.values());
+		configState(IntermissionState.FLAP, this::startStateTimer, this::state_FLAP_update, null);
+		configState(IntermissionState.ACTION, this::startStateTimer, this::state_ACTION_update, null);
 		this.gameController = gameController;
 	}
 
+	/**
+	 * UI provides implementation.
+	 */
 	public abstract void playIntermissionSound();
 
+	/**
+	 * UI provides implementation.
+	 */
 	public abstract void playFlapAnimation();
 
 	public void init() {
 		flap = new Flap(2, "THE CHASE");
 		flap.setPosition(t(3), t(10));
 		flap.visible = true;
-
 		pacMan = new Pac("Pac-Man");
 		pacMan.setDir(Direction.RIGHT);
-
 		msPacMan = new Pac("Ms. Pac-Man");
 		msPacMan.setDir(Direction.RIGHT);
-
-		enter(Phase.FLAP);
+		changeState(IntermissionState.FLAP);
 	}
 
 	public void update() {
-		switch (phase) {
-
-		case FLAP:
-			if (timer.isRunningSeconds(1)) {
-				playFlapAnimation();
-			}
-			if (timer.isRunningSeconds(2)) {
-				flap.visible = false;
-				playIntermissionSound();
-			}
-			if (timer.isRunningSeconds(4.5)) {
-				enter(Phase.ACTION);
-			}
-			timer.tick();
-			break;
-
-		case ACTION:
-			if (timer.isRunningSeconds(1.5)) {
-				pacMan.visible = true;
-				pacMan.setPosition(-t(2), UPPER_Y);
-				pacMan.setDir(Direction.RIGHT);
-				pacMan.setSpeed(2.0);
-				msPacMan.visible = true;
-				msPacMan.setPosition(-t(8), UPPER_Y);
-				msPacMan.setDir(Direction.RIGHT);
-				msPacMan.setSpeed(2.0);
-			}
-			if (timer.isRunningSeconds(6)) {
-				msPacMan.setPosition(t(30), LOWER_Y);
-				msPacMan.visible = true;
-				msPacMan.setDir(Direction.LEFT);
-				msPacMan.setSpeed(2.0);
-				pacMan.setPosition(t(36), LOWER_Y);
-				pacMan.setDir(Direction.LEFT);
-				pacMan.setSpeed(2.0);
-			}
-			if (timer.isRunningSeconds(10.5)) {
-				msPacMan.setPosition(t(-8), MIDDLE_Y);
-				msPacMan.setDir(Direction.RIGHT);
-				msPacMan.setSpeed(2.0);
-				pacMan.setPosition(t(-2), MIDDLE_Y);
-				pacMan.setDir(Direction.RIGHT);
-				pacMan.setSpeed(2.0);
-			}
-			if (timer.isRunningSeconds(14.5)) {
-				msPacMan.setPosition(t(30), UPPER_Y);
-				msPacMan.setDir(Direction.LEFT);
-				msPacMan.setSpeed(4.0);
-				pacMan.setPosition(t(42), UPPER_Y);
-				pacMan.setDir(Direction.LEFT);
-				pacMan.setSpeed(4.0);
-			}
-			if (timer.isRunningSeconds(15.5)) {
-				msPacMan.setPosition(t(-14), LOWER_Y);
-				msPacMan.setDir(Direction.RIGHT);
-				msPacMan.setSpeed(4.0);
-				pacMan.setPosition(t(-2), LOWER_Y);
-				pacMan.setDir(Direction.RIGHT);
-				pacMan.setSpeed(4.0);
-			}
-			if (timer.isRunningSeconds(20)) {
-				gameController.stateTimer().expire();
-				return;
-			}
-			timer.tick();
-			break;
-
-		default:
-			break;
-		}
 		pacMan.move();
 		msPacMan.move();
+		updateState();
+	}
+
+	private void startStateTimer() {
+		stateTimer().start();
+	}
+
+	private void state_FLAP_update() {
+		if (stateTimer().isRunningSeconds(1)) {
+			playFlapAnimation();
+		} else if (stateTimer().isRunningSeconds(2)) {
+			flap.visible = false;
+			playIntermissionSound();
+		} else if (stateTimer().isRunningSeconds(4.5)) {
+			changeState(IntermissionState.ACTION);
+		}
+	}
+
+	private void state_ACTION_update() {
+		if (stateTimer().isRunningSeconds(1.5)) {
+			pacMan.visible = true;
+			pacMan.setPosition(-t(2), UPPER_Y);
+			pacMan.setDir(Direction.RIGHT);
+			pacMan.setSpeed(2.0);
+			msPacMan.visible = true;
+			msPacMan.setPosition(-t(8), UPPER_Y);
+			msPacMan.setDir(Direction.RIGHT);
+			msPacMan.setSpeed(2.0);
+		} else if (stateTimer().isRunningSeconds(6)) {
+			msPacMan.setPosition(t(30), LOWER_Y);
+			msPacMan.visible = true;
+			msPacMan.setDir(Direction.LEFT);
+			msPacMan.setSpeed(2.0);
+			pacMan.setPosition(t(36), LOWER_Y);
+			pacMan.setDir(Direction.LEFT);
+			pacMan.setSpeed(2.0);
+		} else if (stateTimer().isRunningSeconds(10.5)) {
+			msPacMan.setPosition(t(-8), MIDDLE_Y);
+			msPacMan.setDir(Direction.RIGHT);
+			msPacMan.setSpeed(2.0);
+			pacMan.setPosition(t(-2), MIDDLE_Y);
+			pacMan.setDir(Direction.RIGHT);
+			pacMan.setSpeed(2.0);
+		} else if (stateTimer().isRunningSeconds(14.5)) {
+			msPacMan.setPosition(t(30), UPPER_Y);
+			msPacMan.setDir(Direction.LEFT);
+			msPacMan.setSpeed(4.0);
+			pacMan.setPosition(t(42), UPPER_Y);
+			pacMan.setDir(Direction.LEFT);
+			pacMan.setSpeed(4.0);
+		} else if (stateTimer().isRunningSeconds(15.5)) {
+			msPacMan.setPosition(t(-14), LOWER_Y);
+			msPacMan.setDir(Direction.RIGHT);
+			msPacMan.setSpeed(4.0);
+			pacMan.setPosition(t(-2), LOWER_Y);
+			pacMan.setDir(Direction.RIGHT);
+			pacMan.setSpeed(4.0);
+		} else if (stateTimer().isRunningSeconds(20)) {
+			gameController.stateTimer().expire();
+		}
 	}
 }
