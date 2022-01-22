814d23683185b14d8b2391b295ad3195fe4139a5
Armin Reichert <armin.reichert@web.de>
2022-01-22 18:35:23 +0100

    cleanup

--- pacman-core/src/main/java/de/amr/games/pacman/lib/Logging.java
+++ pacman-core/src/main/java/de/amr/games/pacman/lib/Logging.java
@@ -30,19 +30,14 @@ import java.time.format.DateTimeFormatter;
  * Log messages.
  * 
  * @author Armin Reichert
- *
  */
 public class Logging {
 
-	public static boolean enabled = true;
-
 	public static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
-
+	
 	public static void log(String msg, Object... args) {
-		if (enabled) {
-			String timestamp = TIME_FORMAT.format(LocalTime.now());
-			String message = String.format(msg, args);
-			System.err.printf("[%s] %s\n", timestamp, message);
-		}
+		String timestamp = TIME_FORMAT.format(LocalTime.now());
+		String message = String.format(msg, args);
+		System.err.printf("[%s] %s\n", timestamp, message);
 	}
 }
