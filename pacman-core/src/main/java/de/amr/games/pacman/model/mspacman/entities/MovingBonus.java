69ecc152adb1f6814fcf1c85c1d458b5dc52f464
Armin Reichert <armin.reichert@web.de>
2022-02-14 09:31:46 +0100

    change bonus path

--- pacman-core/src/main/java/de/amr/games/pacman/model/mspacman/entities/MovingBonus.java
+++ pacman-core/src/main/java/de/amr/games/pacman/model/mspacman/entities/MovingBonus.java
@@ -26,6 +26,8 @@ package de.amr.games.pacman.model.mspacman.entities;
 import java.util.Random;
 
 import de.amr.games.pacman.lib.Direction;
+import de.amr.games.pacman.lib.Logging;
+import de.amr.games.pacman.lib.V2i;
 import de.amr.games.pacman.model.common.BonusState;
 import de.amr.games.pacman.model.pacman.entities.Bonus;
 import de.amr.games.pacman.model.world.Portal;
@@ -40,10 +42,21 @@ import de.amr.games.pacman.model.world.Portal;
  */
 public class MovingBonus extends Bonus {
 
+	private enum Phase {
+		GO_TO_HOUSE_ENTRY, GO_TO_OTHER_SIDE, GO_TO_HOUSE_ENTRY_AGAIN, LEAVE;
+	}
+
+	private V2i entryTile;
+	private V2i exitTile;
+	private Phase phase;
+
 	@Override
 	public void init() {
 		state = BonusState.INACTIVE;
 		timer = 0;
+		phase = null;
+		entryTile = null;
+		exitTile = null;
 		targetTile = null;
 		newTileEntered = true;
 		forcedOnTrack = true;
@@ -58,22 +71,31 @@ public class MovingBonus extends Bonus {
 		timer = ticks;
 		this.symbol = symbol;
 		this.points = points;
-		// place at random portal tile
-		int numPortals = world.portals().size();
-		Random random = new Random();
-		Portal randomPortal = world.portals().get(random.nextInt(numPortals));
-		if (random.nextBoolean()) {
-			placeAt(randomPortal.left, 0, 0);
-			targetTile = world.portals().get(random.nextInt(numPortals)).right;
+		boolean leftToRight = new Random().nextBoolean();
+		if (leftToRight) {
+			entryTile = randomPortal().left;
+			exitTile = randomPortal().right;
 			setDir(Direction.RIGHT);
 			setWishDir(Direction.RIGHT);
 		} else {
-			placeAt(randomPortal.right, 0, 0);
-			targetTile = world.portals().get(random.nextInt(numPortals)).left;
+			entryTile = randomPortal().right;
+			exitTile = randomPortal().left;
 			setDir(Direction.LEFT);
 			setWishDir(Direction.LEFT);
 		}
+		placeAt(entryTile, 0, 0);
+		setTargetTile(world.ghostHouse().entryTile());
+		phase = Phase.GO_TO_HOUSE_ENTRY;
 		show();
+	}
+
+	private Portal randomPortal() {
+		return world.portals().get(new Random().nextInt(world.portals().size()));
+	}
+
+	private void setTargetTile(V2i tile) {
+		targetTile = tile;
+		Logging.log("Bonus target tile is now %s", targetTile);
 	}
 
 	@Override
@@ -85,10 +107,22 @@ public class MovingBonus extends Bonus {
 		}
 
 		case EDIBLE -> {
-			if (tile().equals(targetTile)) {
-				hide();
+			if (phase == Phase.LEAVE && tile().equals(targetTile)) {
 				state = BonusState.INACTIVE;
+				hide();
 				return true;
+			}
+			if (phase == Phase.GO_TO_HOUSE_ENTRY && tile().equals(targetTile)) {
+				phase = Phase.GO_TO_OTHER_SIDE;
+				setTargetTile(world.ghostHouse().entryTile().plus(0, 6));
+			}
+			if (phase == Phase.GO_TO_OTHER_SIDE && tile().equals(targetTile)) {
+				phase = Phase.GO_TO_HOUSE_ENTRY_AGAIN;
+				setTargetTile(world.ghostHouse().entryTile());
+			}
+			if (phase == Phase.GO_TO_HOUSE_ENTRY_AGAIN && tile().equals(targetTile)) {
+				phase = Phase.LEAVE;
+				setTargetTile(exitTile);
 			}
 			headForTile(targetTile);
 			tryMoving();
@@ -97,8 +131,8 @@ public class MovingBonus extends Bonus {
 
 		case EATEN -> {
 			if (timer == 0) {
-				hide();
 				state = BonusState.INACTIVE;
+				hide();
 				return true;
 			}
 			timer--;
