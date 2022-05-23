3cece0103ef4786b420b9dfa4ca300eb4d0208c4
Armin Reichert <armin.reichert@web.de>
2022-05-23 18:41:54 +0200

    refac

--- pacman-core/src/main/java/de/amr/games/pacman/controller/pacman/Intermission2Controller.java
+++ pacman-core/src/main/java/de/amr/games/pacman/controller/pacman/Intermission2Controller.java
@@ -23,31 +23,133 @@ SOFTWARE.
  */
 package de.amr.games.pacman.controller.pacman;
 
+import static de.amr.games.pacman.model.common.world.World.t;
+
 import de.amr.games.pacman.controller.common.GameController;
+import de.amr.games.pacman.controller.pacman.Intermission2Controller.Context;
+import de.amr.games.pacman.controller.pacman.Intermission2Controller.State;
+import de.amr.games.pacman.lib.Direction;
 import de.amr.games.pacman.lib.Fsm;
+import de.amr.games.pacman.lib.FsmState;
+import de.amr.games.pacman.lib.TickTimer;
+import de.amr.games.pacman.model.common.GameEntity;
+import de.amr.games.pacman.model.common.GameModel;
+import de.amr.games.pacman.model.common.Ghost;
+import de.amr.games.pacman.model.common.GhostState;
+import de.amr.games.pacman.model.common.Pac;
 
 /**
  * Second intermission scene: Blinky pursues Pac but kicks a nail that tears his dress apart.
  * 
  * @author Armin Reichert
  */
-public class Intermission2Controller extends Fsm<Intermission2State, Intermission2Context> {
+public class Intermission2Controller extends Fsm<State, Context> {
+
+	public static class Context {
+		public Runnable playIntermissionSound = Fsm::nop;
+		public Ghost blinky;
+		public Pac pac;
+		public GameEntity nail;
+	}
+
+	public enum State implements FsmState<Intermission2Controller.Context> {
+
+		CHASING {
+			@Override
+			public void onEnter(Context $) {
+				timer.setDurationIndefinite().start();
+
+				$.playIntermissionSound.run();
+
+				$.pac = new Pac("Pac-Man");
+				$.pac.setMoveDir(Direction.LEFT);
+				$.pac.setPosition(t(30), t(20));
+				$.pac.setSpeed(1.0);
+				$.pac.show();
+
+				$.blinky = new Ghost(GameModel.RED_GHOST, "Blinky");
+				$.blinky.state = GhostState.HUNTING_PAC;
+				$.blinky.setMoveDir(Direction.LEFT);
+				$.blinky.setWishDir(Direction.LEFT);
+				$.blinky.position = $.pac.position.plus(t(14), 0);
+				$.blinky.setSpeed(1.0);
+				$.blinky.show();
+
+				$.nail = new GameEntity();
+				$.nail.setPosition(t(14), t(20) - 1);
+				$.nail.show();
+			}
+
+			@Override
+			public void onUpdate(Context $) {
+				if (controller.nailDistance() == 0) {
+					controller.changeState(STRETCHED);
+					return;
+				}
+				$.pac.move();
+				$.blinky.move();
+			}
+		},
+
+		STRETCHED {
+			@Override
+			public void onUpdate(Context $) {
+				int stretching = controller.nailDistance() / 4;
+				if (stretching == 3) {
+					$.blinky.setSpeed(0);
+					$.blinky.setMoveDir(Direction.UP);
+					controller.changeState(State.STUCK);
+					return;
+				}
+				$.blinky.setSpeed(0.3 - 0.1 * stretching);
+				$.blinky.move();
+				$.pac.move();
+			}
+		},
+
+		STUCK {
+			@Override
+			public void onUpdate(Context $) {
+				if (timer.atSecond(2)) {
+					$.blinky.setMoveDir(Direction.RIGHT);
+				} else if (timer.atSecond(6)) {
+					controller.gameController.state().timer().expire();
+					return;
+				}
+				$.blinky.move();
+				$.pac.move();
+			}
+		};
+
+		protected Intermission2Controller controller;
+		protected final TickTimer timer = new TickTimer("Timer:" + name());
+
+		@Override
+		public void setFsm(Fsm<? extends FsmState<Context>, Context> fsm) {
+			controller = (Intermission2Controller) fsm;
+		}
+
+		@Override
+		public TickTimer timer() {
+			return timer;
+		}
+	}
 
 	public final GameController gameController;
-	public final Intermission2Context context = new Intermission2Context();
+	public final Context context = new Context();
 
 	public Intermission2Controller(GameController gameController) {
-		super(Intermission2State.values());
+		super(State.values());
 		this.gameController = gameController;
 	}
 
 	@Override
-	public Intermission2Context getContext() {
+	public Context getContext() {
 		return context;
 	}
 
 	public void init() {
-		reset(Intermission2State.CHASING);
+		reset(State.CHASING);
 	}
 
 	public int nailDistance() {
--- pacman-core/src/main/java/de/amr/games/pacman/controller/pacman/Intermission3Controller.java
+++ pacman-core/src/main/java/de/amr/games/pacman/controller/pacman/Intermission3Controller.java
@@ -23,8 +23,19 @@ SOFTWARE.
  */
 package de.amr.games.pacman.controller.pacman;
 
+import static de.amr.games.pacman.model.common.world.World.t;
+
 import de.amr.games.pacman.controller.common.GameController;
+import de.amr.games.pacman.controller.pacman.Intermission3Controller.Context;
+import de.amr.games.pacman.controller.pacman.Intermission3Controller.State;
+import de.amr.games.pacman.lib.Direction;
 import de.amr.games.pacman.lib.Fsm;
+import de.amr.games.pacman.lib.FsmState;
+import de.amr.games.pacman.lib.TickTimer;
+import de.amr.games.pacman.model.common.GameModel;
+import de.amr.games.pacman.model.common.Ghost;
+import de.amr.games.pacman.model.common.GhostState;
+import de.amr.games.pacman.model.common.Pac;
 
 /**
  * Third intermission scene: Blinky in shred dress chases Pac-Man, comes back half-naked drawing its dress over the
@@ -32,22 +43,91 @@ import de.amr.games.pacman.lib.Fsm;
  * 
  * @author Armin Reichert
  */
-public class Intermission3Controller extends Fsm<Intermission3State, Intermission3Context> {
+public class Intermission3Controller extends Fsm<State, Context> {
+
+	public class Context {
+		public Runnable playIntermissionSound = Fsm::nop;
+		public Ghost blinky;
+		public Pac pac;
+	}
+
+	public enum State implements FsmState<Context> {
+
+		CHASING {
+			@Override
+			public void onEnter(Context $) {
+				timer.setDurationIndefinite().start();
+				$.playIntermissionSound.run();
+
+				$.pac = new Pac("Pac-Man");
+				$.pac.setMoveDir(Direction.LEFT);
+				$.pac.setPosition(t(40), t(20));
+				$.pac.setSpeed(1.2);
+				$.pac.show();
+
+				$.blinky = new Ghost(GameModel.RED_GHOST, "Blinky");
+				$.blinky.state = GhostState.HUNTING_PAC;
+				$.blinky.setMoveDir(Direction.LEFT);
+				$.blinky.setWishDir(Direction.LEFT);
+				$.blinky.position = $.pac.position.plus(t(8), 0);
+				$.blinky.setSpeed(1.2);
+				$.blinky.show();
+			}
+
+			@Override
+			public void onUpdate(Context $) {
+				if ($.blinky.position.x <= -t(15)) {
+					$.pac.setSpeed(0);
+					$.blinky.setMoveDir(Direction.RIGHT);
+					$.blinky.setWishDir(Direction.RIGHT);
+					controller.changeState(RETURNING);
+					return;
+				}
+				$.pac.move();
+				$.blinky.move();
+			}
+		},
+
+		RETURNING {
+			@Override
+			public void onUpdate(Context $) {
+				if ($.blinky.position.x > t(53)) {
+					controller.gameController.state().timer().expire();
+					return;
+				}
+				$.pac.move();
+				$.blinky.move();
+			}
+		};
+
+		protected Intermission3Controller controller;
+		protected final TickTimer timer = new TickTimer("Timer:" + name());
+
+		@Override
+		public void setFsm(Fsm<? extends FsmState<Context>, Context> fsm) {
+			controller = (Intermission3Controller) fsm;
+		}
+
+		@Override
+		public TickTimer timer() {
+			return timer;
+		}
+	}
 
 	public final GameController gameController;
-	public final Intermission3Context context = new Intermission3Context();
+	public final Context context = new Context();
 
 	public Intermission3Controller(GameController gameController) {
-		super(Intermission3State.values());
+		super(State.values());
 		this.gameController = gameController;
 	}
 
 	@Override
-	public Intermission3Context getContext() {
+	public Context getContext() {
 		return context;
 	}
 
 	public void init() {
-		reset(Intermission3State.CHASING);
+		reset(State.CHASING);
 	}
 }
