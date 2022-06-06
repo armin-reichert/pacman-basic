e086ab9da3d60daa093689e0ad9aef4c2a403780
Armin Reichert <armin.reichert@web.de>
2022-06-06 13:51:44 +0200

    enum magic

--- pacman-core/src/main/java/de/amr/games/pacman/model/common/GameVariant.java
+++ pacman-core/src/main/java/de/amr/games/pacman/model/common/GameVariant.java
@@ -23,24 +23,13 @@ SOFTWARE.
  */
 package de.amr.games.pacman.model.common;
 
+import de.amr.games.pacman.lib.EnumMethods;
+
 /**
  * Game variants that can be played.
  * 
  * @author Armin Reichert
  */
-public enum GameVariant {
-
+public enum GameVariant implements EnumMethods<GameVariant> {
 	MS_PACMAN, PACMAN;
-
-	public static int numValues() {
-		return values().length;
-	}
-
-	public GameVariant pred() {
-		return values()[ordinal() == 0 ? numValues() - 1 : ordinal() - 1];
-	}
-
-	public GameVariant succ() {
-		return values()[ordinal() == numValues() - 1 ? 0 : ordinal() + 1];
-	}
 }
