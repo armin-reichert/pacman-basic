4b637c6ac06ab41ef78e3ce0e3b78d42e7f7af2d
Armin Reichert <armin.reichert@web.de>
2022-08-15 08:23:42 +0200

    test

--- pacman-core/src/test/java/de/amr/games/pacman/model/world/WorldTests.java
+++ pacman-core/src/test/java/de/amr/games/pacman/model/world/WorldTests.java
@@ -5,12 +5,28 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import de.amr.games.pacman.lib.V2d;
+import de.amr.games.pacman.lib.V2i;
 import de.amr.games.pacman.model.common.world.ArcadeWorld;
 import de.amr.games.pacman.model.common.world.World;
 import de.amr.games.pacman.model.mspacman.MsPacManGame;
 import de.amr.games.pacman.model.pacman.PacManGame;
 
-public class TestWorldFunctions {
+public class WorldTests {
+
+	@Test
+	public void testTileCoordinates() {
+		V2d pos = new V2d(0.0, 0.0);
+		assertEquals(V2i.NULL, World.tileAt(pos));
+		pos = new V2d(7.9, 7.9);
+		assertEquals(new V2i(0, 0), World.tileAt(pos));
+		pos = new V2d(8.0, 7.9);
+		assertEquals(new V2i(1, 0), World.tileAt(pos));
+		pos = new V2d(8.0, 0.0);
+		assertEquals(new V2i(1, 0), World.tileAt(pos));
+		pos = new V2d(0.0, 8.0);
+		assertEquals(new V2i(0, 1), World.tileAt(pos));
+	}
 
 	@Test
 	public void testPacManWorld() {
