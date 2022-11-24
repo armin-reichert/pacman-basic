0ebd885eced8e48c566c3b0d00cbccd557e2f324
Armin Reichert <armin.reichert@web.de>
2022-11-24 08:40:44 +0100

    refac

--- pacman-core/src/main/java/de/amr/games/pacman/controller/common/GameState.java
+++ pacman-core/src/main/java/de/amr/games/pacman/controller/common/GameState.java
@@ -395,7 +395,7 @@ public enum GameState implements FsmState<GameModel>, GameCommands {
 			game.energizerPulse.advance();
 			game.pac.update(game);
 			if (timer.betweenSeconds(0, 1)) {
-				game.ghosts().forEach(ghost -> ghost.selectedAnimation().ifPresent(EntityAnimation::advance));
+				game.ghosts().forEach(Ghost::advanceAnimation);
 			}
 			if (timer.atSecond(0.25)) {
 				game.pac.selectAndResetAnimation(AnimKeys.PAC_DYING);
