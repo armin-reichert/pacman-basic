2ebe224db483dcd81d13b20f2a39644a8037e25a
Armin Reichert <armin.reichert@web.de>
2023-09-19 09:25:58 +0200

    Refac

--- pacman-core/src/main/java/de/amr/games/pacman/model/world/ArcadeWorld.java
+++ pacman-core/src/main/java/de/amr/games/pacman/model/world/ArcadeWorld.java
@@ -2,6 +2,7 @@ package de.amr.games.pacman.model.world;
 
 import de.amr.games.pacman.lib.Vector2f;
 
+import static de.amr.games.pacman.lib.Globals.checkNotNull;
 import static de.amr.games.pacman.lib.Globals.v2i;
 
 /**
@@ -11,6 +12,10 @@ public class ArcadeWorld extends World {
 
     public static class ArcadeHouse extends House {
 
+        private static final Vector2f SEAT_LEFT   = halfTileRightOf(11, 17);
+        private static final Vector2f SEAT_MIDDLE = halfTileRightOf(13, 17);
+        private static final Vector2f SEAT_RIGHT  = halfTileRightOf(15, 17);
+
         public ArcadeHouse() {
             setMinTile(v2i(10, 15));
             setSize(v2i(8, 5));
@@ -19,10 +24,11 @@ public class ArcadeWorld extends World {
 
         @Override
         public Vector2f seat(String id) {
+            checkNotNull(id);
             switch (id) {
-                case "left":   return halfTileRightOf(11, 17);
-                case "middle": return halfTileRightOf(13, 17);
-                case "right":  return halfTileRightOf(15, 17);
+                case "left":   return SEAT_LEFT;
+                case "middle": return SEAT_MIDDLE;
+                case "right":  return SEAT_RIGHT;
                 default: throw new IllegalArgumentException("Illegal seat ID: " + id);
             }
         }
