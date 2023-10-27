fafca96a1615062ede0d71b400a2b5d03ab37ee7
Armin Reichert <armin.reichert@web.de>
2023-10-27 10:38:17 +0200

    test

--- pacman-core/src/test/java/de/amr/games/pacman/test/HouseTest.java
+++ pacman-core/src/test/java/de/amr/games/pacman/test/HouseTest.java
@@ -7,39 +7,36 @@ package de.amr.games.pacman.test;
 import de.amr.games.pacman.lib.Vector2i;
 import de.amr.games.pacman.model.GameModel;
 import de.amr.games.pacman.model.world.Door;
-import org.junit.Assert;
 import org.junit.Test;
 
 import static de.amr.games.pacman.lib.Globals.halfTileRightOf;
 import static de.amr.games.pacman.lib.Globals.v2i;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 /**
  * @author Armin Reichert
  */
 public class HouseTest {
 
-	static final Vector2i POSITION = v2i(10, 15);
-	static final Vector2i SIZE = v2i(8, 5);
-	static final Door DOOR = new Door(v2i(13, 15), v2i(14, 15));
+	static final Vector2i ARCADE_HOUSE_POSITION = v2i(10, 15);
+	static final Vector2i ARCADE_HOUSE_SIZE = v2i(8, 5);
+	static final Door ARCADE_HOUSE_DOOR = new Door(v2i(13, 15), v2i(14, 15));
 
-	@Test(expected = NullPointerException.class)
-	public void testLeftDoorWingNotNull() {
-		new Door(null, v2i(0, 0));
-	}
-
-	@Test(expected = NullPointerException.class)
-	public void testRightDoorWingNotNull() {
-		new Door(v2i(0, 0), null);
+	@Test
+	public void testDoorWingNotNull() {
+		assertThrows(NullPointerException.class, () -> new Door(null, v2i(0, 0)));
+		assertThrows(NullPointerException.class, () -> new Door(v2i(0, 0), null));
 	}
 
 	@Test
-	public void testHouseProperties() {
+	public void testArcadeHouseProperties() {
 		var house = GameModel.createArcadeHouse();
-		Assert.assertEquals(POSITION, house.topLeftTile());
-		Assert.assertEquals(SIZE, house.size());
-		Assert.assertEquals(DOOR, house.door());
-		Assert.assertEquals(halfTileRightOf(11, 17), house.seat("left"));
-		Assert.assertEquals(halfTileRightOf(13, 17), house.seat("middle"));
-		Assert.assertEquals(halfTileRightOf(15, 17), house.seat("right"));
+		assertEquals(ARCADE_HOUSE_POSITION, house.topLeftTile());
+		assertEquals(ARCADE_HOUSE_SIZE, house.size());
+		assertEquals(ARCADE_HOUSE_DOOR, house.door());
+		assertEquals(halfTileRightOf(11, 17), house.seat("left"));
+		assertEquals(halfTileRightOf(13, 17), house.seat("middle"));
+		assertEquals(halfTileRightOf(15, 17), house.seat("right"));
 	}
 }
